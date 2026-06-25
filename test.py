ab6b3d3006c76ef71ab703fe1fbff30ec58fdac7
Navaneeth <navaneethsnp26@gmail.com>
2026-06-25 11:23:53 +0530

    python_basics

---
+++ test.py
@@ -0,0 +1,24 @@
+print("hello world","world",sep="#")
+#print("hello world",end="")
+#print("hello world")
+x="10.5"
+#y="s"
+#print("abc">"abd")
+#x=int(input())
+#print(int(float(x)))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print("""hello
+#      my name is 
+#      John
+#      """)
+#x="hello"
+#print(x[0:3:2]) #start:end:step
+#print(x[::-1]) #reverse string
+#l=[1,2,3,4,5]
+#print(l[::2]) #start:end:step
+for i in range(0,10,2):
+    print(i)
+#while True:
+#    print("hello")
